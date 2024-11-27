b829be379b4e02528a2547c4bedce6b196017c3f
rupesh-dev30 <cgcrupesh@gmail.com>
2024-11-27 23:08:12 +0530

    change backgroun black and white

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-purple-500">
+    <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-white to-black">
       {children}
     </div>
   );
