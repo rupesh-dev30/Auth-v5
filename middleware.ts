aa9ee6f6bbfa9fdde221d11db26aa96408cd8e11
rupesh-dev30 <cgcrupesh@gmail.com>
2024-11-27 15:15:48 +0530

    authjs + middleware v1

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  console.log("ROUTE: ", req.nextUrl.pathname);
+
+  console.log("IS LOGGED IN: ", isLoggedIn);
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
