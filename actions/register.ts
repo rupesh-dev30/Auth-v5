fba5e0beb9e117199dc3f067e3aeebe55df92957
rupesh-dev30 <cgcrupesh@gmail.com>
2024-11-28 17:05:49 +0530

    verification email send: RESEND

--- actions/register.ts
+++ actions/register.ts
@@ -6,6 +6,7 @@ import bcryptjs from "bcryptjs";
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
 import { generateVerificationToken } from "@/lib/token";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -35,7 +36,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const verificationToken = await generateVerificationToken(email);
 
-  //TODO: Send Verification token email
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
   return { success: "Confirmation email sent!" };
 };
